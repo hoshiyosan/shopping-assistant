e856a5f1759ac7988465e47a409f468545140228
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-02-15 16:58:45 +0100

    add cors to backend

--- backend/shopping_assistant/shopping_assistant/__init__.py
+++ backend/shopping_assistant/shopping_assistant/__init__.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from shopping_assistant import domain
+from shopping_assistant.settings import APP_CORS_ORIGINS
 
 api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=APP_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(domain.authnz.router, tags=["Authentication"])
 api.include_router(domain.accounts.router, tags=["Accounts"])
