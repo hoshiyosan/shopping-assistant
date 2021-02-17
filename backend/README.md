b5a6e406ab208159471c4371894251ed68c068db
Sylvan Le Deunff <sledeunf@gmail.com>
2021-02-17 16:40:00 +0100

    allow setting CORS from environment variables

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+
+## Docker Image
+
+### Environment variables
+
+|variable|description|
+|-|-|
+|APP_CORS_ORIGINS|A coma-separated list of domains allowed to send XHR to this backend.|
+|APP_ENCRYPTION_ALGORITHM|Algorithm used to cipher JWToken (default: HS256)|
+|APP_SECRET_KEY|A secret string used as a salt in API ciphering|
--- backend/shopping_assistant/shopping_assistant/settings.py
+++ backend/shopping_assistant/shopping_assistant/settings.py
@@ -3,11 +3,7 @@ from os import getenv as _getenv
 
 APP_ENCRYPTION_ALGORITHM = _getenv("APP_ENCRYPTION_ALGORITHM", "HS256")
 APP_SECRET_KEY = _getenv("APP_SECRET_KEY", "d7da8827-b5d7-4820-9528-e501aa3ab787")
-APP_CORS_ORIGINS = [
-    "http://localhost:8080",
-    "http://192.168.56.124:8080",
-]
-
+APP_CORS_ORIGINS = _getenv("APP_CORS_ORIGINS", "http://localhost:8080,http://192.168.56.124:8080").split(",")
 INGREDIENTS_UNITS = ('grammes', 'millilitres', 'kilogrammes', 'litres', 'cuillere à soupe', 'cuillère à café')
 INGREDIENTS_CATEGORIES = ('Viande', 'Poisson', 'Fruits et Légumes', 'Epicerie sucrée', 'Epicerie salée', 'Boulangerie', 'Fromage')
 
